a31a73a8601785f43e155cd02902dc98ea70bcaa
BalrajBemainboina <48517486+BalrajBemainboina@users.noreply.github.com>
2022-08-18 00:03:25 +0530

    Update samplebrowsertest.py

--- samplebrowsertest.py
+++ samplebrowsertest.py
@@ -54,7 +54,7 @@ def run_session(cap):
         raise Exception("Unable to load myPKFiT page!")
     elem = driver.find_element(By.LINK_TEXT, "India (English)")
     elem.click()
-    driver.find_element(By.LINK_TEXT, "Confirm").click()
+    driver.find_element_by_css_selector("button[ng-click='vm.confirm(vm.selectedCountry)']").click()
     try:
         WebDriverWait(driver, 5).until(EC.title_contains("myPKFiT"))
         driver.execute_script(
