5573462e9532a90742640141f1f6f7347af27e8d
BalrajBemainboina <48517486+BalrajBemainboina@users.noreply.github.com>
2022-08-25 17:48:35 +0530

    Update myPKFitApptest.py

--- myPKFitApptest.py
+++ myPKFitApptest.py
@@ -33,29 +33,22 @@ elements_list = driver.find_elements_by_class_name("android.widget.TextView")
 for element in elements_list:
     if 'Contact Us' in element.text: 
         element.click()
-        break
         print(element.text + ' Selected')
+        break
 time.sleep(2)
 elements_listCancel = driver.find_elements_by_class_name("android.widget.TextView")
 for element in elements_listCancel:
     if 'Cancel' in element.text: 
         element.click()
-        break
         print('Cancel' + ' Selected')
+        break
 time.sleep(2)
 elements_listsign = driver.find_elements_by_class_name("android.widget.TextView")
 for element1 in elements_listsign:
-    print (element1.text)
     if 'Sign Up' in element1.text: 
         element1.click()
-        break
         print('Sign Up' + ' Selected')
-time.sleep(2)
-for element in elements_listCancel:
-    if 'Cancel' in element.text: 
-        element.click()
         break
-        print('Cancel' + ' Selected')
 # If you have uploaded your app, write your test case here. 
 time.sleep(2) 
 # Invoke driver.quit() after the test is done to indicate that the test is completed.
